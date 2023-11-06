0cea779788e8ac01c078bfd1704c9806bdc8798b
Fatma Hussein <fartunhussein346@gmail.com>
2023-11-06 15:24:29 +0300

    fix linter errors

--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "Mains", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/main/home"
+RSpec.describe 'Mains', type: :request do
+  describe 'GET /home' do
+    it 'returns http success' do
+      get '/main/home'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /users" do
-    it "returns http success" do
-      get "/main/users"
+  describe 'GET /users' do
+    it 'returns http success' do
+      get '/main/users'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /post" do
-    it "returns http success" do
-      get "/main/post"
+  describe 'GET /post' do
+    it 'returns http success' do
+      get '/main/post'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /postid" do
-    it "returns http success" do
-      get "/main/postid"
+  describe 'GET /postid' do
+    it 'returns http success' do
+      get '/main/postid'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
