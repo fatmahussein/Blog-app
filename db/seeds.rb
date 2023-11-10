dbb5081ecfbdf659472628995aca62a3b93b5f8b
Fatma Hussein <fartunhussein346@gmail.com>
2023-11-10 22:55:57 +0300

    Add styling to the form and modify routes.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: second_user, title: 'Hello', text: 'This is my first post')
+Comment.create(post: first_post, user: second_user, text: 'Hi Tom!' )
+Comment.create(post: second_post, user: first_user, text: 'Hi Tom!' )
