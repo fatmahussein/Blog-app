c93c359cdf1619862e4dc8e687a92b93c92dec5d
Fatma Hussein <fartunhussein346@gmail.com>
2023-11-14 23:08:02 +0300

    Fix n+1 issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   def index
     @user = User.find_by_id(params[:user_id])
-    @posts = @user.posts if @user # if there is a user, show only their posts
+    @posts = @user.posts.includes(:comments) if @user # if there is a user, show only their posts
   end
 
   def show
     @user = User.find_by_id(params[:user_id])
-  # @post = Post.find_by_id(params[:id])
-    @posts = @user.posts.includes(:comments)
+    @post = Post.find_by_id(params[:id])
+    
   end
 
   def new
