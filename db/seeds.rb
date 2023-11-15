f7ef3e13cff3d1f9b7126aeaa052e3d21867095b
Fatma Hussein <fartunhussein346@gmail.com>
2023-11-15 15:28:16 +0300

    Add changes to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,3 @@ first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0Bx
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
 first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
 second_post = Post.create(author: second_user, title: 'Hello', text: 'This is my first post')
-Comment.create(post: first_post, user: second_user, text: 'Hi Tom!' )
-Comment.create(post: second_post, user: first_user, text: 'Hi Tom!' )
