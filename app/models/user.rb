8f423bc0c9a5868e48702015f7befd6de2ffacd0
Fatma Hussein <fartunhussein346@gmail.com>
2023-11-15 22:00:43 +0300

    Add more modules for the devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
