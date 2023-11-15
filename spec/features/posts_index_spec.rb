06e9f8655e50cb7fdb6ca3c171c9fb85753dd3cb
Mozamel Jawad <mozameljawad@gmail.com>
2023-11-15 12:46:30 +0430

    test for display posts

--- spec/features/posts_index_spec.rb
+++ spec/features/posts_index_spec.rb
@@ -11,4 +11,18 @@ RSpec.feature 'User Profile Page' do
     expect(page).to have_content('Bio')
     expect(page).to have_content('A test bio')
   end
+
+    scenario 'displays user posts when present' do
+    post = create(:post, user: user, title: 'First Post', text: 'Content of the first post')
+    create(:comment, user: user, post: post, text: 'A comment on the first post')
+
+    visit user_path(user)
+
+    expect(page).to have_content('First Post')
+    expect(page).to have_content('Content of the first post')
+    expect(page).to have_content('Comment: 1, Likes: 0') # Assuming counters default to 0
+    expect(page).to have_content('A comment on the first post')
+  end
+
+
 end
